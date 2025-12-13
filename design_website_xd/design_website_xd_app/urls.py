d2058a79a98a90b46b055cfd4be22d1add255fb5
unknown <zarina_ufa@outlook.com>
2025-12-13 12:51:26 +0500

    появилась страница регистраций

--- design_website_xd/design_website_xd_app/urls.py
+++ design_website_xd/design_website_xd_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('auth/', views.auth, name='auth')
+    path('reg/', views.reg, name='reg')
 ]
