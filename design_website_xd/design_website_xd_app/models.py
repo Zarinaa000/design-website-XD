5a10c6fbd519e467e0552d76f7afb8b2fbd3cdcb
unknown <zarina_ufa@outlook.com>
2026-01-24 12:57:07 +0500

    создала законо акк в джанго

--- design_website_xd/design_website_xd_app/models.py
+++ design_website_xd/design_website_xd_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# Service - услуга
+class Service(models.Model):
+    #CharField(max_lenght - 25)
+    #IntegerField()
+    #DataField()
+    #DateTimeField()
+    #FilePathField() - путь до файла
+
+    service_title = models.CharField(max_length= 100) # название продукта
+    service_price = models.FloatField() # цена продукта
+    service_Description = models.TextField() # описание продукта
+    service_image = models.ImageField() # картинка продукта
+    service_quantity = models.IntegerField() # кол-во продукта
+
+    def __str__(self):
+        return f'{self.service_title}'
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Create your models here.
--- design_website_xd/design_website_xd_app/views.py
+++ design_website_xd/design_website_xd_app/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from .models import Service
 
 def index(request):
     try:
@@ -51,4 +52,8 @@ def reg(request):
 def logout_view(request):
     logout(request)
     return redirect('index')
-   
+
+def service_template(request, id):
+   service = Service.objects.get(id = id)
+   context = { 'title' : service.service_title } 
+   return render(request, 'service-template.html', context)
