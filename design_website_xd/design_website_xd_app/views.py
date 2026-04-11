6df36a436eafc5c29682827ddec57c1b37e32804
unknown <zarina_ufa@outlook.com>
2026-04-11 12:55:02 +0500

    в views.py добавиля доп код

--- design_website_xd/design_website_xd_app/views.py
+++ design_website_xd/design_website_xd_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .models import Service
 
 def index(request):
@@ -52,29 +52,44 @@ def logout_view(request):
     logout(request)
     return redirect('index')
 
-def catalog_view(request):
-    service = service.objects.filter(service_type = '')
+def catalog_view(request, catalog_type):
+    catalog_type_name = ''
+
+    if catalog_type == 'all':
+        service = Service.objects.all()
+        catalog_type_name = "Все"
+    else:
+        service = Service.objects.filter(catalog_type = 'catalog_type')
+
+        catalog_types = service.catalog_types
+        
+        for ft in catalog_types:
+            if ft[0] == catalog_type:
+                catalog_type_name = ft[1]
+                break
+
     context = {
-        'service_list': catalog,
+        'service_list' : service,
+        'catalog_type' : catalog_type_name
     }
     return render(request,'catalog.html', context)
 
 def service_template(request, id):
-   service = Service.objects.get(id = id)
+   service = Service.objects.get(id = id) # конструктор класса
    context = {
        'service' : service
     }
    return render(request, 'service-template.html', context)
 
 def account(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        first_name = request.POST.get('firstName')
-        last_name = request.POST.get('lastName')
-        birthday = request.POST.get('birthday')
-        username = email
-
-        return JsonResponse({'status': 'success'})
-
-    return render(request, 'account.html')
+  print(request.user.id)
+  try:
+    context = {
+        'username' : request.user.username,
+        'first_name' : request.user.first_name,
+        'last_name' : request.user.last_name,
+        'email': request.user.email,
+    }
+    return render(request, 'account.html', context)
+  except AttributeError:
+      return HttpResponse("<h1>401 Unauthorized</h1>", status=401)
